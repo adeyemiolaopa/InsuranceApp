f8568d06d6adc8e80bd800c2ba9bb9e68c6a1db8
Adeyemi Olaopa <adeyemi.olaopa@sterling.ng>
2023-11-07 12:54:00 +0100

    Moq Nugget Added

--- InsuranceApp.Tests/UnitTest1.cs
+++ InsuranceApp.Tests/UnitTest1.cs
@@ -1,18 +1,40 @@
+using InsuranceApp.Application.Interfaces;
+using InsuranceApp.Core.Models;
+using Moq;
 using NUnit.Framework;
+using System;
 
 namespace InsuranceApp.Tests
 {
     public class Tests
     {
+        private Mock<IClaimService> _claimService;
+        private Mock<IPolicyHolderService> _policyHolderService;
+
         [SetUp]
         public void Setup()
         {
+            _claimService = new Mock<IClaimService>();
+            _policyHolderService = new Mock<IPolicyHolderService>();
         }
 
         [Test]
-        public void Test1()
+        public void AddClaimAsync_ClaimCreated()
         {
-            Assert.Pass();
+            //Arrange
+            var request = new Claim() 
+            { 
+                ClaimId = Guid.NewGuid(), 
+                PolicyHolderNationalId = "TEST12166", 
+                CreatedDate = DateTime.Now, 
+                Status = ClaimStatus.Submitted, 
+            };
+
+            //Act
+            var response = _claimService.Setup(O => O.AddClaimAsync(request).Result);
+
+            //Assert
+            //Assert.Pass(response);
         }
     }
 }
